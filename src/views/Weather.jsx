10ba36919285abe7ab22d3bdfa52af5d841d3fa5
CalixCarlo <carloalbis59@gmail.com>
2023-04-04 04:34:02 -0700

    4th error

--- src/views/Weather.jsx
+++ src/views/Weather.jsx
@@ -1,25 +1,33 @@
-import React, { useState } from 'react'
-import { Container, Row, Col, Form, Card } from 'react-bootstrap'
-import axios from 'axios'
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Card } from 'react-bootstrap';
+import axios from 'axios';
 
 function Weather() {
-  const [data, setData] = useState({})
-  const [location, setLocation] = useState('')
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
 
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`;
 
   const searchLocation = async (event) => {
     if (event.key === 'Enter') {
       try {
-        const response = await axios.get(url)
-        setData(response.data)
-        console.log(response.data)
+        const response = await axios.get(url);
+        setData(response.data);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-      setLocation('')
+      setLocation('');
     }
-  }
+  };
+
+  const getFiveDayForecast = () => {
+    if (!data.list) return [];
+    const forecasts = data.list.filter(forecast => {
+      const date = new Date(forecast.dt_txt);
+      return date.getHours() === 12 && date.getDate() > new Date().getDate();
+    });
+    return forecasts.slice(0, 5);
+  };
 
   return (
     <Container fluid className="app">
@@ -28,10 +36,11 @@ function Weather() {
           <Form.Group controlId="locationInput">
             <Form.Control
               value={location}
-              onChange={event => setLocation(event.target.value)}
+              onChange={(event) => setLocation(event.target.value)}
               onKeyPress={searchLocation}
-              placeholder='Enter Location'
-              type="text" />
+              placeholder="Enter Location"
+              type="text"
+            />
           </Form.Group>
         </Col>
       </Row>
@@ -40,25 +49,39 @@ function Weather() {
           <Col>
             <div className="top">
               <div className="location">
-                <p>{data.city.name}, {data.city.country}</p>
+                <p>
+                  {data.city.name}, {data.city.country}
+                </p>
               </div>
             </div>
             <Row>
-              {data.list.slice(0, 8).map((forecast, index) => (
+              {getFiveDayForecast().map((forecast, index) => (
                 <Col key={index}>
                   <Card className="text-center">
                     <Card.Body>
-                      <Card.Title>{new Date(forecast.dt_txt).toLocaleDateString()}</Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted">{new Date(forecast.dt_txt).toLocaleTimeString()}</Card.Subtitle>
+                      <Card.Title>
+                        {new Date(forecast.dt_txt).toLocaleDateString()}
+                      </Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted">
+                        {new Date(forecast.dt_txt).toLocaleTimeString()}
+                      </Card.Subtitle>
                       <Card.Text>
-                        <img src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`} alt={forecast.weather[0].description} />
+                        <img
+                          src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`}
+                          alt={forecast.weather[0].description}
+                        />
                         {forecast.weather[0].description}
                       </Card.Text>
                       <Card.Text>{forecast.main.temp.toFixed()}°F</Card.Text>
-                      <Card.Text>Feels Like: {forecast.main.feels_like.toFixed()}°F</Card.Text>
-                      <Card.Text>Humidity: {forecast.main.humidity}%</Card.Text>
-                      <Card.Text>Wind Speed: {forecast.wind.speed.toFixed()} MPH</Card.Text>
-                      
+                      <Card.Text>
+                        Feels Like: {forecast.main.feels_like.toFixed()}°F
+                      </Card.Text>
+                      <Card.Text>
+                        Humidity: {forecast.main.humidity}%
+                      </Card.Text>
+                      <Card.Text>
+                        Wind Speed: {forecast.wind.speed.toFixed()} MPH
+                      </Card.Text>
                     </Card.Body>
                   </Card>
                 </Col>
