bca04e25b24a0fb5c750f1bc3789896c0168a71d
CalixCarlo <carloalbis59@gmail.com>
2023-04-09 23:54:39 -0700

    add styling

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -10,8 +10,8 @@ const Topbar = () => {
       <Navbar.Brand href="/weather">My Weather App</Navbar.Brand>
       <Nav className="me-flex-end">
       <Nav.Link href="/">Search For Weather</Nav.Link>
-      <Nav.Link href="/news">News</Nav.Link>
-      <Nav.Link href="/about">About</Nav.Link>
+      <Nav.Link to="/news">News</Nav.Link>
+      <Nav.Link to="/about">About</Nav.Link>
       </Nav>
     </Container>
   </Navbar>
